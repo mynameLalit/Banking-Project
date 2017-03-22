37300837f7b4412f753f528d0948687aa9ab643d
Shital <Shital@192.168.2.14>
2017-03-22 12:09:06 +0530

    Validation requirtement : Work in progress

--- Personal Banking Project/src/com/banking/controller/LoginServlet.java
+++ Personal Banking Project/src/com/banking/controller/LoginServlet.java
@@ -25,14 +25,41 @@ public class LoginServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//response.getWriter().append("Served at: ").append(request.getContextPath());
 		
-	Customer customer = new Customer();
-	customer.setFirstname(request.getParameter("username"));
+	/*Customer customer = new Customer();
+	customer.setUsername(request.getParameter("username"));
 	customer.setPassword(request.getParameter("password"));
-
-	}	
+	customer.setFirstname(request.getParameter("firstname"));
+	customer.setLastname(request.getParameter("lastname"));
+	
+		customer.
+	*/
+	}
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		doGet(request, response);
+		//doGet(request, response);
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		String firstname = request.getParameter("firstname");
+		String lastname = request.getParameter("lastname");
+		String errorMsg = null;
+		
+		if(username ==null || username.equals("")) {
+			errorMsg = "Username can't be null or empty";
+		}
+		
+		if(password == null || password.equals("")) {
+			errorMsg = "Password can't be null or empty";
+		}
+		
+		if(firstname == null || firstname.equals("")) {
+			errorMsg = "Firstname can't be null or empty";
+		}
+		
+		if(lastname == null || lastname.equals("")) {
+			errorMsg = "Lastname can't be null or empty";
+		}
 	}
 
 }
